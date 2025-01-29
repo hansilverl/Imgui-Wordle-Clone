a7375c7ccec43078739cc7ab207c300f1371c8ef
hansilverl <hannahsilverb@gmail.com>
2025-01-29 14:37:09 +0200

    Add hover effect for keyboard

--- winapp/ConnectedApp/OnScreenKb.cpp
+++ winapp/ConnectedApp/OnScreenKb.cpp
@@ -1,6 +1,5 @@
-﻿// OnScreenKb.cpp
-#include "OnScreenKb.h"
-
+﻿#include "OnScreenKb.h"
+#include "Colors.h"
 
 ImVec4 OnScreenKeyboard::GetKeyColor(char key, const GameLogic& game_logic) const {
     bool found_green = false;
@@ -46,7 +45,7 @@ void OnScreenKeyboard::Render(char* inputBuffer, bool& invalidWord, GameLogic& g
     ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 4.0f);
 
     RenderRow(keys1, inputBuffer, invalidWord, game_logic);
-    ImGui::Dummy(ImVec2(25, 0));    // Add some padding to center the row
+    ImGui::Dummy(ImVec2(25, 0)); // Add some padding to center the row
     ImGui::SameLine(0, 6.0f);
     RenderRow(keys2, inputBuffer, invalidWord, game_logic);
 
@@ -60,17 +59,16 @@ void OnScreenKeyboard::Render(char* inputBuffer, bool& invalidWord, GameLogic& g
     ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.6f, 0.6f, 0.6f, 1.0f));
     ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.4f, 0.4f, 0.4f, 1.0f));
     ImGui::PushFont(ImGui::GetIO().Fonts->Fonts[3]);
-    if (ImGui::Button("x", ImVec2(75, 60))) {   // x maps to backspace in our custom font
+    if (ImGui::Button("x", ImVec2(75, 60))) { // x maps to backspace in our custom font
         if (strlen(inputBuffer) > 0) {
             inputBuffer[strlen(inputBuffer) - 1] = '\0';
         }
     }
     ImGui::PopFont();
     // Restore button style
+    ImGui::PopStyleColor(3);
 
-    ImGui::PopStyleColor(3);    
-
-    ImGui::PopStyleVar();   // 
+    ImGui::PopStyleVar();
     ImGui::End();
 }
 
@@ -78,9 +76,12 @@ void OnScreenKeyboard::RenderRow(const char* keys, char* inputBuffer, bool& inva
     ImGui::PushFont(ImGui::GetIO().Fonts->Fonts[1]);
     for (int i = 0; i < strlen(keys); ++i) {
         ImVec4 keyColor = GetKeyColor(keys[i], game_logic);
+        ImVec4 keyColorHovered = ImVec4(keyColor.x * 1.2f, keyColor.y * 1.2f, keyColor.z * 1.2f, keyColor.w);
+        ImVec4 keyColorActive = ImVec4(keyColor.x * 0.8f, keyColor.y * 0.8f, keyColor.z * 0.8f, keyColor.w);
+
         ImGui::PushStyleColor(ImGuiCol_Button, keyColor);
-        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, keyColor);
-        ImGui::PushStyleColor(ImGuiCol_ButtonActive, keyColor);
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, keyColorHovered); // Lighter color on hover
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, keyColorActive); // Darker color on active
 
         if (ImGui::Button(std::string(1, keys[i]).c_str(), ImVec2(45, 60))) {
             if (strlen(inputBuffer) < 5) {
@@ -89,14 +90,14 @@ void OnScreenKeyboard::RenderRow(const char* keys, char* inputBuffer, bool& inva
             }
         }
         ImGui::PopStyleColor(3);
-        // If not the last key in row , add spacing between buttons
+        // If not the last key in row, add spacing between buttons
         if (i < strlen(keys) - 1) ImGui::SameLine(0, 6.0f);
     }
     ImGui::PopFont();
 }
 
 void OnScreenKeyboard::RenderEnterButton(char* inputBuffer, bool& invalidWord, GameLogic& game_logic) {
-    ImGui::PushFont(ImGui::GetIO().Fonts->Fonts[2]);    // Use a smaller font for the "Enter" button
+    ImGui::PushFont(ImGui::GetIO().Fonts->Fonts[2]); // Use a smaller font for the "Enter" button
     ImGui::PushStyleColor(ImGuiCol_Button, DEFAULT_COLOR);
     ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.6f, 0.6f, 0.6f, 1.0f));
     ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.4f, 0.4f, 0.4f, 1.0f));
@@ -109,6 +110,6 @@ void OnScreenKeyboard::RenderEnterButton(char* inputBuffer, bool& invalidWord, G
         }
     }
 
-	ImGui::PopStyleColor(3);  
+    ImGui::PopStyleColor(3);
     ImGui::PopFont();
 }
